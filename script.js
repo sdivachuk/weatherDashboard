4c70f0b3f2a915489c70f086ae2b3744fc93d51e
Sergey Divachuk <sergeydivachuk@gmail.com>
2022-06-02 22:33:52 -0400

    added css and js

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 var cityNameEl = document.querySelector("#cityName");
 var userInputEl = document.querySelector("#userInput");
+var wetherUrl = https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=55785c8317220fbb9098ae56f9cdac87&units=imperial
+
+
+
+userInputEl.addEventListener('submit', formSubmitHandler);
