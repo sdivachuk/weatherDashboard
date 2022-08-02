fe326107bcba04b0e72fa9921b68b6feba4f80cd
Sergey Divachuk <sergeydivachuk@gmail.com>
2022-08-01 22:21:04 -0400

    added functions for get city, get weather, and show weather

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # weatherDashboard
+
+## Table of Contents
+- [Description](#description)
+- [Acceptance Criteria](#Acceptance)
+- [Visuals](Visuals)
+- [Resources](Resources)
+
+## Description
+
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+
+## Acceptance Criteria
+
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+
+## Visuals
+- [video]( https://www.awesomescreenshot.com/video/10055701?key=15719bf514264f6e7f03505d114730e1)
+
+## Resources
+- [GitHub](https://sdivachuk.github.io/weatherDashboard/)
+- [Repository](https://github.com/sdivachuk/weatherDashboard)
--- script.js
+++ script.js
@@ -1,7 +1,91 @@
 var cityNameEl = document.querySelector("#cityName");
 var userInputEl = document.querySelector("#userInput");
-var wetherUrl = https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=55785c8317220fbb9098ae56f9cdac87&units=imperial
+var weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=55785c8317220fbb9098ae56f9cdac87&units=imperial`;
+
+var GetWeather = function (value) {
+  fetch(weatherUrl).then(function (response) {
+    return response.json();
+  });
+};
+
+function getCity(event) {
+  event.preventDefault();
+  getWeather(cityNameEl.value);
+}
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  var cityName = cityNameEl.value.trim();
+
+  if (cityName) {
+    getWeather(cityName);
+
+    weatherContainerEl.textContent = "";
+    cityNameEl.value = "";
+  } else {
+    alert("No city with that name found!");
+  }
+};
+
+var buttonHandler = function (event) {
+  var city = event.target.getAttribute("search-city");
+
+  if (city) {
+    getWeather(city);
+    weatherContainerEl.textContent = "";
+  }
+};
+
+var getCitysWeather = function (value) {
+  fetch(weatherUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      showWeather(lat, lon);
+      storeCity(value);
+    });
+};
+
+function showWeather(lat, lon, timezone) {
+  var date = dayjs().tz(timezone).format("M/D/YYYY");
+  fetch(weatherUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    forecastContainerEl.innerHTML = '';
+    var temp = data.current.temp;
+    var wind = data.current.wind_speed;
+    var uv = data.current.uvi;
+    var humidity = data.current.humidity;
+    var containerEl = document.createElement('div');
+    var h2El = document.createElement('h2');
+    var tempEl = document.createElement('p');
+    var windEl = document.createElement('p');
+    var uvEl = document.createElement('p');
+    var humEl = document.createElement('p');
+
+
+    tempEl.textContent = "Temperature: " + temp;
+    windEl.textContent = "Wind Speed: " + wind;
+    uvEl.textContent = "UV Index: " + uv;
+    humEl.textContent = "Humidity: " + humidity;
+
+    h2El.textContent = date
+
+    containerEl.append(h2El, tempEl, windEl, uvEl, humEl);
+    weatherContainerEl.append(containerEl);
+
+  })
+}
 
 
 
-userInputEl.addEventListener('submit', formSubmitHandler);
+
+
+
+
+userInputEl.addEventListener("submit", formSubmitHandler);
